b1d74a3c66db0c138925af9c80eb4882752e4ec1
mohammad <mohammad19nour@gmailcom>
2024-02-12 10:57:26 +0400

    s

--- API/Controllers/User/SubscriptionController.cs
+++ API/Controllers/User/SubscriptionController.cs
@@ -1,4 +1,4 @@
-﻿using AsparagusN.Data.Entities.MealPlan.Admin;
+﻿/*using AsparagusN.Data.Entities.MealPlan.Admin;
 using AsparagusN.Data.Entities.MealPlan.UserPlan;
 using AsparagusN.DTOs.UserPlanDtos;
 using AsparagusN.Entities.Identity;
@@ -53,100 +53,12 @@ public class SubscriptionController : BaseApiController
         return Ok(plan);
     }
 
-    private async Task<bool> _addDrinksToDaysPlan(List<Item> userSelectedDrinks, UserPlan plan)
-    {
-        try
-        {
-            var adminDrinks = await _unitOfWork.Repository<AdminSelectedDrink>().GetQueryable()
-                .Where(x => x.PlanTypeEnum == plan.PlanType).Include(x => x.Drink).ToListAsync();
+    
 
-            var drinksToAdd = new List<UserSelectedDrink>();
-            foreach (var drink in userSelectedDrinks)
-            {
-                var dr = adminDrinks.FirstOrDefault(x => x.DrinkId == drink.Id);
-                if (dr == null) return false;
-                drinksToAdd.Add(_mapper.Map<UserSelectedDrink>(dr.Drink));
-            }
-
-            foreach (var day in plan.Days)
-            {
-                day.SelectedDrinks = drinksToAdd;
-            }
-
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-            throw;
-        }
-    }
-
-    private async Task<bool> _addExtrasToDaysPlan(List<Item> userSelectedExtras, UserPlan plan)
-    {
-        try
-        {
-            var adminExtras = await _unitOfWork.Repository<AdminSelectedExtraOption>().GetQueryable()
-                .Where(x => x.PlanTypeEnum == plan.PlanType).Include(x => x.ExtraOption).ToListAsync();
-
-            var extrasToAdd = new List<UserSelectedExtraOption>();
-            foreach (var extra in userSelectedExtras)
-            {
-                var dr = adminExtras.FirstOrDefault(x => x.ExtraOptionId == extra.Id);
-                if (dr == null) return false;
-                var toAdd = _mapper.Map<UserSelectedExtraOption>(dr.ExtraOption);
-                toAdd.Weight = extra.Quantity;
-                extrasToAdd.Add(toAdd);
-            }
-
-            foreach (var day in plan.Days)
-            {
-                day.SelectedExtraOptions = extrasToAdd;
-            }
-
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-            throw;
-        }
-    }
-
-    private bool _addPlanDaysToPlan(NewSubscriptionDto subscriptionDto, UserPlan plan)
-    {
-        try
-        {
-            var days = new List<UserPlanDay>();
-
-            while (subscriptionDto.Duration > 0)
-            {
-                subscriptionDto.Duration--;
-                var day = subscriptionDto.StartDate.AddDays(1);
-
-                days.Add(new UserPlanDay
-                {
-                    Day = day,
-                });
-                subscriptionDto.StartDate = day;
-            }
-
-            plan.Days = days;
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-            throw;
-        }
-    }
-
+    
     private async Task<AppUser?> _getUser()
     {
         var email = HttpContext.User.GetEmail();
         return await _userManager.Users.FirstOrDefaultAsync(x => x.Email == email);
     }
-}
+}*/
--- API/Services/SubscriptionService.cs
+++ API/Services/SubscriptionService.cs
@@ -1,4 +1,4 @@
-﻿using AsparagusN.Data.Entities.MealPlan.Admin;
+﻿/*using AsparagusN.Data.Entities.MealPlan.Admin;
 using AsparagusN.Data.Entities.MealPlan.UserPlan;
 using AsparagusN.DTOs.UserPlanDtos;
 using AsparagusN.Entities.Identity;
@@ -28,22 +28,29 @@ public class SubscriptionService : ISubscriptionService
     {
         if (await CheckExistingSubscriptionPlanForUserAsync(user.Id, subscriptionDto.PlanType))
             return null;
-        
+
         var plan = _mapper.Map<UserPlan>(subscriptionDto);
         plan.User = user;
-     
-        var ok = _addPlanDaysToPlan(subscriptionDto, plan);
+
+        var ok = AddPlanDaysToPlan(subscriptionDto, plan);
         if (subscriptionDto.SelectedDrinks != null)
         {
             var drinkIds = subscriptionDto.SelectedDrinks.Select(x => x.Id).ToList();
-            if (await CheckExistingDrinks(drinkIds,subscriptionDto.PlanType))
+            if (await CheckExistingDrinks(drinkIds, subscriptionDto.PlanType))
             {
-              await  AddDrinksToDaysPlan(subscriptionDto.SelectedDrinks,plan);
+                ok &= await AddDrinksToDaysPlan(subscriptionDto.SelectedDrinks, plan);
             }
             else return null;
         }
+
         if (subscriptionDto.SelectedExtras != null)
-            ok &= await _addExtrasToDaysPlan(subscriptionDto.SelectedExtras, plan);
+        {
+            if (await CheckExistingExtras(subscriptionDto.SelectedExtras.Select(x => x.Id).ToList(),
+                    subscriptionDto.PlanType))
+            {
+                ok &= await AddExtrasToDaysPlan(subscriptionDto.SelectedExtras, plan);
+            }
+        }
     }
 
     public async Task<bool> CheckExistingSubscriptionPlanForUserAsync(int userId, PlanTypeEnum planType)
@@ -53,22 +60,17 @@ public class SubscriptionService : ISubscriptionService
 
         return plan != null;
     }
-    
+
     private async Task<bool> AddDrinksToDaysPlan(List<Item> userSelectedDrinks, UserPlan plan)
     {
         try
         {
             var drinkIds = userSelectedDrinks.Select(x => x.Id).ToList();
             var adminDrinks = await _unitOfWork.Repository<AdminSelectedDrink>().GetQueryable()
-                .Where(x => x.PlanTypeEnum == plan.PlanType && drinkIds.Contains(x.Id) ).Include(x => x.Drink).ToListAsync();
+                .Where(x => x.PlanTypeEnum == plan.PlanType && drinkIds.Contains(x.Id)).Include(x => x.Drink)
+                .ToListAsync();
 
-            var drinksToAdd = new List<UserSelectedDrink>();
-            foreach (var drink in adminDrinks)
-            {
-                var tmp = _mapper.Map<UserSelectedDrink>(drink.Drink);
-                
-                drinksToAdd.Add(tmp);
-            }
+            var drinksToAdd = adminDrinks.Select(drink => _mapper.Map<UserSelectedDrink>(drink.Drink)).ToList();
 
             foreach (var day in plan.Days)
             {
@@ -85,6 +87,71 @@ public class SubscriptionService : ISubscriptionService
         }
     }
 
+    private async Task<bool> AddExtrasToDaysPlan(List<Item> userSelectedExtras, UserPlan plan)
+    {
+        try
+        {
+            var extraIds = userSelectedExtras.Select(x => x.Id).ToList();
+            var adminExtras = await _unitOfWork.Repository<AdminSelectedExtraOption>().GetQueryable()
+                .Where(x => x.PlanTypeEnum == plan.PlanType && extraIds.Contains(x.Id)).Include(x => x.ExtraOption)
+                .ToListAsync();
+
+            var extrasToAdd = new List<UserSelectedExtraOption>();
+            foreach (var extra in userSelectedExtras)
+            {
+                var dr = adminExtras.First(x => x.ExtraOptionId == extra.Id);
+
+                var toAdd = _mapper.Map<UserSelectedExtraOption>(dr.ExtraOption);
+                toAdd.Weight = extra.Quantity;
+                extrasToAdd.Add(toAdd);
+            }
+
+            foreach (var day in plan.Days)
+            {
+                day.SelectedExtraOptions = extrasToAdd;
+            }
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+            throw;
+        }
+    }
+
+
+    private bool AddPlanDaysToPlan(NewSubscriptionDto subscriptionDto, UserPlan plan)
+    {
+        try
+        {
+            var days = new List<UserPlanDay>();
+
+            while (subscriptionDto.Duration > 0)
+            {
+                subscriptionDto.Duration--;
+                var day = subscriptionDto.StartDate.AddDays(1);
+
+                days.Add(new UserPlanDay
+                {
+                    Day = day,
+                });
+                subscriptionDto.StartDate = day;
+            }
+
+            plan.Days = days;
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+            throw;
+        }
+    }
+
+
     private async Task<bool> CheckExistingDrinks(ICollection<int> ids, PlanTypeEnum planType)
     {
         var adminDrinks = await _unitOfWork.Repository<AdminSelectedDrink>().GetQueryable()
@@ -92,6 +159,7 @@ public class SubscriptionService : ISubscriptionService
 
         return adminDrinks.Count == ids.Count;
     }
+
     private async Task<bool> CheckExistingExtras(ICollection<int> ids, PlanTypeEnum planType)
     {
         var adminExtras = await _unitOfWork.Repository<AdminSelectedExtraOption>().GetQueryable()
@@ -99,4 +167,4 @@ public class SubscriptionService : ISubscriptionService
 
         return adminExtras.Count == ids.Count;
     }
-}
+}*/
